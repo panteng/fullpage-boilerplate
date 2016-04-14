919e02c246aecf7862f69cafe186166264d780c9
Chris Pan <chrispan.pt@gmail.com>
2016-04-14 16:08:14 +0800

    Add section text and btn-next. Remove favicon.

--- javascripts/main.js
+++ javascripts/main.js
@@ -4,8 +4,29 @@ var $ = require('jquery');
 var fullpage = require('fullpage.js');
 
 $(document).ready(function () {
+    var sectionsLength = $('.section').length;
+    
     $('#fullpage').fullpage({
         menu: '#menu',
-        paddingTop: 60
-    });      
+        paddingTop: 60, // this value should be the same as the header's height (60px).
+        afterLoad: function (anchorLink, index) {
+            switch (index) {
+                case 1: 
+                    $('#btn-next').show();
+                    break;
+                
+                case 2:
+                    $('#btn-next').show();
+                    break;
+                    
+                case 3:
+                    $('#btn-next').hide();
+                    break;
+            }
+        }
+    });
+    
+    $('#btn-next').on('click', function () {
+        $.fn.fullpage.moveSectionDown();
+    });
 });
